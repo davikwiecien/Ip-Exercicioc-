a372dd8755fb7832e293a2d57192f1bfa32d1c77
davikwiecien <66526045+davikwiecien@users.noreply.github.com>
2021-05-31 23:11:09 -0300

    Update exercicio3.c

--- exercicio3.c
+++ exercicio3.c
@@ -7,12 +7,14 @@ valores, então, extraia a raiz quarta. Apresente o resultado do cálculo ao usu
 //exempo - gcc exercicio3.c -o exercicio3 -lm
 //-lm é a flag
 
-float main()
-{
+int main() {
+	
 	float v1, v2, v3, v4, v5, soma, raiz;
 	printf("insira 5 valores para a serem somados e dados a raiz quadrada\n");
 	scanf("%f %f %f %f %f", &v1, &v2, &v3, &v4, &v5),
 	soma=v1+v2+v3+v4+v5;
 	raiz= sqrt(soma);
 	printf("so= %f\n",raiz);
+	
+	return 0;
 }
